f303c5ff0c37ddbfb728581b0b319d7141cf1467
darkone0112 <darkone0112@gmail.com>
2023-04-25 21:47:16 +0200

    new repo

--- PullAllRemote.sh
+++ PullAllRemote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # list of repositories to pull
-repositories=("Autogit" "CevaLogisticsInventaryApp" "DAM1" "DAM2" "darkone0112" "DLX-Patcher" "JavaFX" "JdbcDaoProject" "pippin-bot" "Python" "legacy-databases-migration")
+repositories=("Autogit" "CevaLogisticsInventaryApp" "DAM1" "DAM2" "darkone0112" "DLX-Patcher" "JavaFX" "JdbcDaoProject" "pippin-bot" "Python" "legacy-databases-migration" "ad-harvesting-app")
   cd .. # go back to the parent directory
 # loop through repositories and pull latest changes
 for repo in "${repositories[@]}"
--- PushAllRemote.sh
+++ PushAllRemote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # list of repositories to push
-repositories=("Autogit" "CevaLogisticsInventaryApp" "DAM1" "DAM2" "darkone0112" "DLX-Patcher" "JavaFX" "JdbcDaoProject" "pippin-bot" "Python" "legacy-databases-migration")
+repositories=("Autogit" "CevaLogisticsInventaryApp" "DAM1" "DAM2" "darkone0112" "DLX-Patcher" "JavaFX" "JdbcDaoProject" "pippin-bot" "Python" "legacy-databases-migration" "ad-harvesting-app")
 
 # loop through repositories and push latest changes
 cd ..
